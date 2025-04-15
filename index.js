4b514a08e61892b4302723950eab3427fb81a05e
AlmaCRH <alma5crh@gmail.com>
2025-04-15 14:13:08 +0100

    mansheai action

---
+++ index.js
@@ -0,0 +1,19 @@
+import * as core from "@actions/core";
+import runMansheAI from "mansheai";
+    const [githubUser, githubRepo] = process.env.GITHUB_REPOSITORY.split("/");
+
+(async () => {
+  try {
+    const githubToken = core.getInput("github_token");
+    const aikey = core.getInput("ai_key");
+
+    await runMansheAI({
+      githubToken,
+      aikey,
+      githubUser,
+      githubRepo,
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
