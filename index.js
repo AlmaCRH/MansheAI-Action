be7be4422518bfa72f29b922a4535bcfaa10cf8f
AlmaCRH <alma5crh@gmail.com>
2025-04-15 16:05:15 +0100

    added

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import * as core from "@actions/core";
 import runMansheAI from "mansheai";
 const [githubUser, githubRepo] = process.env.GITHUB_REPOSITORY.split("/");
-
+const githubToken = process.env.GITHUB_TOKEN
 (async () => {
   try {
-    const mycode = core.getInput("my_code");
     const aikey = core.getInput("ai_key");
 
     await runMansheAI({
-      mycode,
+      githubToken,
       aikey,
       githubUser,
       githubRepo,
